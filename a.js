c13a30a73b092589fc22656978cd718aad235765
ankursasmal <ankursasmal2017@gmail.com>
2023-12-12 23:31:59 +0530

    wild life wed site

---
+++ a.js
@@ -0,0 +1,8 @@
+   let c=document.querySelector("#lo");
+   c.addEventListener("click",()=>{
+      alert("login here");
+   });
+   let d=document.querySelector("#sin");
+   d.addEventListener("click",()=>{
+      alert("signup here");
+   });
